ced8ea5b6e763b2a783792a583c5aedc943324e2
Sara Fernández Ramos <sararunda@hotmail.com>
2022-03-21 11:23:47 +0100

    updated exercise

--- js/main.js
+++ js/main.js
@@ -1,5 +1,48 @@
 "use strict";
-const form1 = document.querySelector (".js-newCollapsed");
+const form1 = document.querySelector(".js-newCollapsed");
 form1.classList.remove("collapsed");
+const list = document.querySelector(".js-list");
+const
+const kitten1 = `<li class="card">
+<article>
+  <img
+    class="card_img"
+    src="https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg"
+    alt="gatito"
+  />
+  <h3 class="card_title">Anastacio</h3>
+  <h4 class="card_race">British Shorthair</h4>
+  <p class="card_description">
+    Ruiseño, juguetón, le guta estar tranquilo y que nadie le
+    moleste. Es una maravilla acariciarle!
+  </p>
+</article>
+</li>`;
+const kitten2 = `<li class="card js-kitten1">
+<img
+  class="card_img"
+  src="https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg"
+  alt="gatito"
+/>
+<h3 class="card_title">Fiona</h3>
+<h4 class="card_race">British Shorthair</h4>
+<p class="card_description">
+  Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.
+  Es una maravilla acariciarle!
+</p>
+</li>`;
+const kitten3 = `<li class="card js-kitten2">
+<img
+  class="card_img"
+  src="https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg"
+  alt="gatito"
+/>
+<h3 class="card_title">Cielo</h3>
+<h4 class="card_race">British Shorthair</h4>
+<p class="card_description">
+  Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.
+  Es una maravilla acariciarle!
+</p>
+</li>`;
 
 //let list1 = document.querySelector (".new-forcollapsed");
